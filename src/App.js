a7ec9aa04d0921ebd7150260048222fab11730b5
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-20 19:33:16 -0400

    UI is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div id="display">
+          <div className="equation">5 x 2</div>
+          <div className="result">10</div>
+        </div>
+        <div className="btns">
+          <div className="row first-row">
+            <button id="clear">AC</button>
+            <button id="divide">÷</button>
+            <button id="multiply">X</button>
+          </div>
+          <div className="row second-row">
+            <button id="seven">7</button>
+            <button id="eight">8</button>
+            <button id="nine">9</button>
+            <button id="subtract">-</button>
+          </div>
+          <div className="row third-row">
+            <button id="four">4</button>
+            <button id="five">5</button>
+            <button id="six">6</button>
+            <button id="add">+</button>
+          </div>
+          <div className="row fourth-row">
+            <button id="one">1</button>
+            <button id="two">2</button>
+            <button id="three">3</button>
+          </div>
+          <div className="row fifth-row">
+            <button id="zero">0</button>
+            <button id="decimal">.</button>
+            <button id="equals">=</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
