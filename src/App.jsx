7ff4e1d9536433e7d40451a8f923379db9149511
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-25 16:51:40 -0400

    working on logic

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,38 @@ function App() {
   const [display, setDisplay] = useState("");
   const [operators, setOperators] = useState([]);
 
+  const regEx = /\d|[*\-/+.]/;
+  const mainOperators = /[*/\-+]/;
+  const latestOperator = display[display.length - 1];
+
+  const input = display.split(mainOperators);
+  const latestInput = +input[input.length - 1];
+  const prevOperator = operators[0];
+
+  const calculationHandler = () => {
+    setResult((prev) =>
+      prev === null
+        ? latestInput
+        : prevOperator === "+"
+        ? (prev += latestInput)
+        : prevOperator === "-"
+        ? (prev -= latestInput)
+        : prevOperator === "/"
+        ? (prev /= latestInput)
+        : (prev *= latestInput)
+    );
+  };
+
+  const clearHandler = () => {
+    setDisplay("");
+    setResult(0);
+    setOperators([]);
+    console.clear();
+  };
+
   const keydownHandler = (e) => {
-    const regEx = /\d|[*\-/+.]/;
-    const mainOperators = /[*/\-+]/;
-    const latestOperator = display[display.length - 1];
+    //display has a bug
+    //1-1+100-99-10+10/2+2/2+5/6-1+
     const latestKeyboardInput = e.key;
 
     if (
@@ -31,37 +59,23 @@ function App() {
     if (mainOperators.test(latestKeyboardInput)) {
       setOperators([latestKeyboardInput, ...operators]);
 
-      const input = display.split(mainOperators);
-      const latestInput = +input[input.length - 1];
-      const prevOperator = operators[0];
-
-      setResult((prev) =>
-        prev === null
-          ? latestInput
-          : prevOperator === "+"
-          ? (prev += latestInput)
-          : prevOperator === "-"
-          ? (prev -= latestInput)
-          : prevOperator === "/"
-          ? (prev /= latestInput)
-          : (prev *= latestInput)
-      );
+      calculationHandler();
     }
 
-    if (latestKeyboardInput === BACK_SPACE || e.target.innerText === AC) {
-      setDisplay("");
-      setResult(0);
-      setOperators([]);
-      console.clear();
+    if (latestKeyboardInput === BACK_SPACE) {
+      clearHandler();
     }
 
     if (/=/.test(latestKeyboardInput) || e.target.innerText === EQUALS) {
-      setDisplay(result);
+      calculationHandler();
     }
+  };
 
-    // if (regEx.test(e.target.innerText)) {
-    //   setDisplay((prev) => (prev += e.target.innerText));
-    // }
+  const btnClickHandler = (e) => {
+    if (regEx.test(e.target.innerText))
+      setDisplay((prev) => (prev += e.target.innerText));
+
+    if (e.target.innerText === AC) clearHandler();
   };
 
   useEffect(() => {
@@ -81,68 +95,71 @@ function App() {
         </div>
         <div className="btns">
           <div className="row first-row">
-            <button id="clear" onClick={keydownHandler}>
+            <button id="clear" onClick={btnClickHandler}>
               AC
             </button>
-            <button id="divide" onClick={keydownHandler}>
+            <button id="divide" onClick={btnClickHandler}>
               /
             </button>
-            <button id="multiply" onClick={keydownHandler}>
+            <button id="multiply" onClick={btnClickHandler}>
               *
             </button>
           </div>
           <div className="row second-row">
-            <button id="seven" onClick={keydownHandler}>
+            <button id="seven" onClick={btnClickHandler}>
               7
             </button>
-            <button id="eight" onClick={keydownHandler}>
+            <button id="eight" onClick={btnClickHandler}>
               8
             </button>
-            <button id="nine" onClick={keydownHandler}>
+            <button id="nine" onClick={btnClickHandler}>
               9
             </button>
-            <button id="subtract" onClick={keydownHandler}>
+            <button id="subtract" onClick={btnClickHandler}>
               -
             </button>
           </div>
           <div className="row third-row">
-            <button id="four" onClick={keydownHandler}>
+            <button id="four" onClick={btnClickHandler}>
               4
             </button>
-            <button id="five" onClick={keydownHandler}>
+            <button id="five" onClick={btnClickHandler}>
               5
             </button>
-            <button id="six" onClick={keydownHandler}>
+            <button id="six" onClick={btnClickHandler}>
               6
             </button>
-            <button id="add" onClick={keydownHandler}>
+            <button id="add" onClick={btnClickHandler}>
               +
             </button>
           </div>
           <div className="row fourth-row">
-            <button id="one" onClick={keydownHandler}>
+            <button id="one" onClick={btnClickHandler}>
               1
             </button>
-            <button id="two" onClick={keydownHandler}>
+            <button id="two" onClick={btnClickHandler}>
               2
             </button>
-            <button id="three" onClick={keydownHandler}>
+            <button id="three" onClick={btnClickHandler}>
               3
             </button>
           </div>
           <div className="row fifth-row">
-            <button id="zero" onClick={keydownHandler}>
+            <button id="zero" onClick={btnClickHandler}>
               0
             </button>
-            <button id="decimal" onClick={keydownHandler}>
+            <button id="decimal" onClick={btnClickHandler}>
               .
             </button>
-            <button id="equals" onClick={keydownHandler}>
+            <button id="equals" onClick={btnClickHandler}>
               =
             </button>
           </div>
         </div>
       </div>
+      <footer>
+        Developed by <em> Hossein Keshtkar</em>. <br /> August 2023
+      </footer>
     </div>
   );
 }
