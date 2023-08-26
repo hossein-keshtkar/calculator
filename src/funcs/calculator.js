5c090d106affaa785e8b5be7083a15a18badbe82
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-25 21:33:36 -0400

    calculator updated

--- src/funcs/calculator.js
+++ src/funcs/calculator.js
@@ -1,6 +1,6 @@
 export const calculator = (num1, num2, operator) => {
-  if (operator === "+") return num1 + num2;
-  if (operator === "-") return num1 - num2;
-  if (operator === "/") return num1 / num2;
-  if (operator === "*") return num1 * num2;
+  if (operator === "+") return +num1 + +num2;
+  if (operator === "-") return +num1 - +num2;
+  if (operator === "/") return +num1 / +num2;
+  if (operator === "*") return +num1 * +num2;
 };
