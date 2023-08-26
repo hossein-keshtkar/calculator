8db9c729cb8fafdf97c571427300d05f028bef8f
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-25 21:42:12 -0400

    displays functionality updated

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,7 +6,9 @@ const Display = ({ state }) => {
   return (
     <div id="display" className={styles.container}>
       <div className={styles.equation}>
-        {state.num1 + state.operator + state.num2}
+        {`${state.num1 !== null ? state.num1 : ""} ${
+          state.operator !== null ? state.operator : ""
+        } ${state.num2 !== null ? state.num2 : ""}`}
       </div>
       <div className={styles.result}>
         {state.result === null ? 0 : state.result}
